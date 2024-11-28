ecd8389887281270e23ef7db024410961be94f8d
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-11-28 23:45:03 +0530

    Dunith

---
+++ law_connect/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-red-500'>
+    <Routes>
+          <Route path='/' element={<Home/>}/>
+
+          
+
+        </Routes>
+    </div>
+
+  )
+}
+
+export default App
