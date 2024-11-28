cfb6b6612770e19a85c8af4d4265f9737fdaccd1
chiranjaya15 <chiranjayadidula1@gmail.com>
2024-11-29 00:41:34 +0530

    commitment

--- law_connect/src/App.jsx
+++ law_connect/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-red-500'>
     <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/> \\this is a commit 
 
           
 
