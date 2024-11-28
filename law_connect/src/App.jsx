ef877248862da74add6c08f10cd47bb0868c2513
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-11-29 01:15:25 +0530

    d

--- law_connect/src/App.jsx
+++ law_connect/src/App.jsx
@@ -10,11 +10,7 @@ function App() {
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-red-500'>
     <Routes>
           <Route path='/' element={<Home/>}/> \\this is a commit 
-
-
-
-
-        </Routes>
+    </Routes>
     </div>
 
   )
