546d0df652eb9b4700175ccb6aca55d345eccd57
GWeerasooriya <gweerasooriya34@gmail.com>
2024-11-28 22:45:35 +0530

    dis

--- law_connect/src/App.jsx
+++ law_connect/src/App.jsx
@@ -10,9 +10,6 @@ function App() {
   return (
     <>
       <div>
-      <div>
-        <useHistory/>
-    </div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
