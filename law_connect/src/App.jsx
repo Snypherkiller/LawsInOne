4b88a6ef1a1f01ed5cf6c8d658356ccc921f7a97
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-11-29 17:58:36 +0530

    k

--- law_connect/src/App.jsx
+++ law_connect/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
  import { Routes,Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Navbar from './Component/Navbar'
+import Login from './Pages/Login'
 
 function App() {
 
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-red-500'>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-red-100'>
+      <Navbar/>
     <Routes>
-          <Route path='/' element={<Home/>}/> \\this is a commit 
+          <Route path='/' element={<Home/>}/> 
+          <Route path='/login' element={<Login/>}/> 
+          <Route path='/use history' element={}/>
     </Routes>
     </div>
 
---
+++ law_connect/src/Pages/useHistory.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useHistory = () => {
+  return (
+    <div>
+      User history
+    </div>
+  )
+}
+
+export default useHistory
+
